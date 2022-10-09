884ffdf98579f8ae28315b8787d9a054fbf536f1
mauricio@maupc.mauriciodemouralima@gmail.com <mauricio@maupc.mauriciodemouralima@gmail.com>
2022-10-09 09:45:59 +0200

    2]

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ from matplotlib import image
 from run_eval import ret
 from model_unet import HackathonModel
 import torch
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as
 
 """
 # Welcome to Silos Detect by Brasil'IA
@@ -22,7 +22,8 @@ if uploaded_file is not None:
     img = image.imread(uploaded_file)
     #img = np.load(uploaded_file)
 
-    st.write(ret(img)[0])
+    st.write("Silos presence probability")
+    st.write(ret(img)[0].item())
 
     input = defaultdict()
     input['img'] = torch.tensor(img[None, :])
